28826fec37733969a92515029045540d167023aa
Ina Evangelista <ina_evangelista@dlsu.edu.ph>
2021-08-19 03:05:08 +0800

    add album

--- app/src/main/java/com/mobdeve/s18/recordnest/SubmitAlbumActivity.java
+++ app/src/main/java/com/mobdeve/s18/recordnest/SubmitAlbumActivity.java
@@ -1,4 +1,125 @@
 package com.mobdeve.s18.recordnest;
 
-public class SubmitAlbumActivity {
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.mobdeve.s18.recordnest.databinding.ActivitySubmitAlbumBinding;
+
+import java.io.IOException;
+
+public class SubmitAlbumActivity extends AppCompatActivity{
+    private ActivitySubmitAlbumBinding binding;
+
+    public static final String KEY_User_Document1 = "doc1";
+    ImageView id_cover;
+    Button btn_upload;
+
+    private String Document_img1="";
+
+    private Bitmap alteredImage, bitmap;
+    private Canvas canvas;
+    private Paint paint;
+    private Matrix matrix;
+
+    private float windowWidth;
+    private float windowHeight;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySubmitAlbumBinding.inflate(getLayoutInflater());
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        //setContentView(R.layout.activity_main);
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        id_cover= findViewById(R.id.iv_cover);
+        btn_upload= findViewById(R.id.btn_upload_cover);
+
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+
+            }
+        });
+
+        //btn_upload.setOnClickListener(this);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav);
+
+        bottomNavigationView.setSelectedItemId(R.id.invisible);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(),UserProfileActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.search:
+                        startActivity(new Intent(getApplicationContext(),SearchUserActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+
+    }
+
+
+    private void selectImage() {
+        Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Uri selectedImage;
+
+        selectedImage = data == null ? null : data.getData();
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+            bitmap = Bitmap.createScaledBitmap(bitmap,  600 ,600, true);
+            id_cover.setImageBitmap(bitmap); //trying bitmap
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
 }
