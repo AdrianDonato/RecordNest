c75b8daf014b1df77dbad8a4ea4a38475625e0c6
AdrianDonato <67679435+AdrianDonato@users.noreply.github.com>
2021-08-25 13:30:46 +0800

    other user - load from database

--- app/src/main/java/com/mobdeve/s18/recordnest/OtherUserProfileActivity.java
+++ app/src/main/java/com/mobdeve/s18/recordnest/OtherUserProfileActivity.java
@@ -8,16 +8,27 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.mobdeve.s18.recordnest.adapter.CollectionAdapter;
 import com.mobdeve.s18.recordnest.adapter.UserListAdapter;
 import com.mobdeve.s18.recordnest.databinding.ActivityOtherUserProfileBinding;
 import com.mobdeve.s18.recordnest.model.Collection;
+import com.mobdeve.s18.recordnest.model.User;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
@@ -25,8 +36,11 @@ public class OtherUserProfileActivity extends AppCompatActivity {
 
     private ActivityOtherUserProfileBinding binding;
 
-    TextView username;
-    ImageView userimg;
+    private TextView username, followercount, followingcount;
+    private ImageView userimg;
+    private ArrayList<Collection> collArray;
+    private FirebaseFirestore fStore;
+    private String userID;
 
     CollectionAdapter collectionAdapter;
 
@@ -42,9 +56,12 @@ public class OtherUserProfileActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        fStore = FirebaseFirestore.getInstance();
 
         this.username = findViewById(R.id.tv_other_username);
         this.userimg = findViewById(R.id.iv_other_userImage);
+        this.followercount = findViewById(R.id.tv_ou_followers);
+        this.followingcount = findViewById(R.id.tv_ou_following);
 
         Intent i = getIntent();
 
@@ -53,6 +70,9 @@ public class OtherUserProfileActivity extends AppCompatActivity {
 
         this.username.setText(name);
         this.userimg.setImageResource(cover);
+
+        this.userID = "4F41MMsEjSWDxzkVHcTDvlj8BLD3";
+        initializeUserData(userID);
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav);
@@ -80,7 +100,63 @@ public class OtherUserProfileActivity extends AppCompatActivity {
             }
         });
 
-        collectionAdapter = new CollectionAdapter(getApplicationContext(), initializeData());
+        initializeDataCollection();
+    }
+
+    public void initializeUserData(String userID){
+        fStore.collection("UserDetails").document(userID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    DocumentSnapshot snapshot = task.getResult();
+                    String retUsername = snapshot.getString("Username");
+                    String retImgURL = snapshot.getString("ProfPicURL");
+                    int retFollowers = snapshot.getLong("FollowerCount").intValue();
+                    int retFollowing = snapshot.getLong("FollowingCount").intValue();
+
+                    username.setText(retUsername);
+                    followercount.setText(Integer.toString(retFollowers));
+                    followingcount.setText(Integer.toString(retFollowing));
+
+                    if(!(retImgURL.equals("placeholder"))){
+                        Glide.with(getApplicationContext()).load(retImgURL).into(userimg);
+                    }
+
+                } else {
+                    Toast.makeText(OtherUserProfileActivity.this, "Error! " + task.getException().getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void initializeDataCollection() {
+        collArray = new ArrayList<>();
+        fStore.collection("AlbumCollection")
+                .whereEqualTo("UserID", userID).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot documentSnapshot : task.getResult()){
+                                String collID = documentSnapshot.getId();
+                                String collTitle = documentSnapshot.getString("Title");
+
+                                collArray.add(new Collection(collTitle));
+                                collArray.get(collArray.size()-1).setCollectionID(collID);
+                            }
+                            initCollAdapter();
+                        } else {
+                            Toast.makeText(OtherUserProfileActivity.this, "Error! " + task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    //initializes collection adapter
+    public void initCollAdapter(){
+        collectionAdapter = new CollectionAdapter(getApplicationContext(), collArray);
 
         LinearLayoutManager lm = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
 
@@ -97,5 +173,9 @@ public class OtherUserProfileActivity extends AppCompatActivity {
         data.add(new Collection("Collection 3"));
 
         return data;
+    }
+
+    public void followUser(){
+
     }
 }
